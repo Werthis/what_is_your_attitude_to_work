04dab6a613dcd15c3fc4826db8c6773179fd329e
Janko Walski <lukaszstawecki@gmail.com>
2021-10-03 16:55:25 +0200

    #1 Done

---
+++ tests/read_test.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+sys.path.append(".")
+from src.read_data import ReadDataFrame
+
+FILE_PATH = 'per_capita_income_data.csv'
+
+class TestPoint(unittest.TestCase):
+
+    def test_same(self):
+        country_name = 'Poland'
+        k = ReadDataFrame(country_name)
+        self.assertEqual(k.iterate_on_rows(), '4578')
+
+
+if __name__ == "__main__":
+    unittest.main()
