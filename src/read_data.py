ca28ade1e0b85e813bb53f48358db34dc2762037
Janko Walski <lukaszstawecki@gmail.com>
2021-10-05 22:46:21 +0200

    #3 Done

--- src/read_data.py
+++ src/read_data.py
@@ -1,41 +1,96 @@
 from csv import reader
 import numpy as np
+from forex_python.converter import CurrencyRates
 
-FILE_PATH = 'per_capita_income_data.csv'
 
-class ReadDataFrame():
+
+PER_CAPITA_FILE_PATH = 'per_capita_income_data.csv'
+CURRENCY_CODES_FILE_PATH = 'currency_codes.csv'
+
+class ReadPerCapitaFile():
 
     def __init__(self, country):
         self.country = country
+        self.title_country = self.country.title()
 
-    def open_data_file(self):
-        self.data_file = open(FILE_PATH)
-        return self.data_file
+    def open_per_capita_data_file(self):
+        self.per_capita_data_file = open(PER_CAPITA_FILE_PATH)
+        return self.per_capita_data_file
 
-    def read_data_file(self):
-        self.data_file_read = reader(self.data_file)
-        return self.data_file_read
+    def read_per_capita_data_file(self):
+        self.per_capita_data_file_read = reader(self.per_capita_data_file)
+        return self.per_capita_data_file_read
 
     def iterate_on_rows(self):
-        for row in self.data_file_read:
-            if self.country in row:
+        for row in self.per_capita_data_file_read:
+            if self.title_country in row:
                 # print(row[2])
                 return row[2]
+            for i in row:
+                if self.title_country in i:
+                    # print(row[2])
+                    return row[2]
+    def close_files(self):
+        self.per_capita_data_file.close()
+
+
+class ReadCurrencyCode():
+
+    def __init__(self, country):
+        self.country = country
+        self.upper_country = self.country.upper()
+
+    def open_currency_data_file(self):
+        self.currency_data_file = open(CURRENCY_CODES_FILE_PATH)
+        return self.currency_data_file
+
+    def read_currency_data_file(self):
+        self.currency_data_file_read = reader(self.currency_data_file)
+        return self.currency_data_file_read
+
+    def iterate_on_rows(self):
+        for row in self.currency_data_file_read:
+            if self.upper_country in row:
+                # print(row[2])
+                return row[2]
+            for i in row:
+                if self.upper_country in i:
+                    # print(row[2])
+                    return row[2]
 
     def close_files(self):
-        self.data_file.close()
+        self.currency_data_file.close()
+
+
+
+c = CurrencyRates()
+
+# print(c.get_rate('USD', 'GBP'))
 
 class StartProgram():
 
     def __init__(self):
         user_input = input('Write your country: ')
 
-        program = ReadDataFrame(user_input)
-        program.open_data_file()
-        program.read_data_file()
-        program.iterate_on_rows()
+        get_per_capita_value = ReadPerCapitaFile(user_input)
+        get_per_capita_value.open_per_capita_data_file()
+        get_per_capita_value.read_per_capita_data_file()
+        per_capita = get_per_capita_value.iterate_on_rows()
+        get_per_capita_value.close_files()
 
-        program.close_files()
+        get_currency_code = ReadCurrencyCode(user_input)
+        get_currency_code.open_currency_data_file()
+        get_currency_code.read_currency_data_file()
+        currency = get_currency_code.iterate_on_rows()
+        get_currency_code.close_files()
+
+        c = CurrencyRates()
+         
+        rate = c.get_rate('USD', currency)
+        final = rate * float(per_capita)
+        print(f'Your country per capita is {per_capita} USD and the currency is {currency}, so the per capita in your currency is: {final}.')
+
+
 
 
 if __name__ == "__main__":
--- tests/read_test.py
+++ tests/read_test.py
@@ -1,25 +1,47 @@
 import unittest
 import sys
 sys.path.append(".")
-from src.read_data import ReadDataFrame
+from src.read_data import ReadPerCapitaFile, ReadCurrencyCode
 
-FILE_PATH = 'per_capita_income_data.csv'
+PER_CAPITA_FILE_PATH = 'per_capita_income_data.csv'
 
 class TestPoint(unittest.TestCase):
 
     def test_per_capita_file(self):
         country_name = 'Poland'
-        k = ReadDataFrame(country_name)
-        k.open_data_file()
-        k.read_data_file()
+        k = ReadPerCapitaFile(country_name)
+        k.open_per_capita_data_file()
+        k.read_per_capita_data_file()
         self.assertEqual(k.iterate_on_rows(), '4578')
         k.close_files()
 
         country_name = 'Iran'
-        k = ReadDataFrame(country_name)
-        k.open_data_file()
-        k.read_data_file()
+        k = ReadPerCapitaFile(country_name)
+        k.open_per_capita_data_file()
+        k.read_per_capita_data_file()
         self.assertEqual(k.iterate_on_rows(), '3115')
+        k.close_files()
+
+    def test_currency_file(self):
+        country_name = 'Poland'
+        k = ReadCurrencyCode(country_name)
+        k.open_currency_data_file()
+        k.read_currency_data_file()
+        self.assertEqual(k.iterate_on_rows(), 'PLN')
+        k.close_files()
+
+        country_name = 'Australia'
+        k = ReadCurrencyCode(country_name)
+        k.open_currency_data_file()
+        k.read_currency_data_file()
+        self.assertEqual(k.iterate_on_rows(), 'AUD')
+        k.close_files()
+
+        country_name = 'Iran'
+        k = ReadCurrencyCode(country_name)
+        k.open_currency_data_file()
+        k.read_currency_data_file()
+        self.assertEqual(k.iterate_on_rows(), 'IRR')
         k.close_files()
 
 if __name__ == "__main__":
